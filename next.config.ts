5387885b8f19c2e527719c47c96ca8f90b0056e4
hyogeun <stonepagoda8@gmail.com>
2025-01-20 21:45:28 +0900

    chore: 외부 이미지 사용을 위한 도메인 설정

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      "ui-avatars.com", // 기본 아바타 이미지
+      "lh3.googleusercontent.com", // 실제 사용자 이미지 도메인
+    ],
+  },
 };
 
 export default nextConfig;
