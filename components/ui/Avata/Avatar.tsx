6422e7f581275726aee1b8d6497cbb1401aee765
hyogeun <stonepagoda8@gmail.com>
2025-03-03 03:25:57 +0900

    feat: Avatar 컴포넌트 개선 및 클라이언트 사이드 렌더링 최적화
    - AVATAR_SIZE 상수에 픽셀 크기 추가
    - 클라이언트 사이드 렌더링 상태 관리를 위한 isClient 상태 도입
    - 이미지 품질 및 크기 설정 최적화
    - 동적 이미지 로딩 및 에러 처리 로직 개선

--- components/ui/Avata/Avatar.tsx
+++ components/ui/Avata/Avatar.tsx
@@ -2,13 +2,16 @@
 
 import { CurrentUserType } from "@/types/authTypes";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const AVATAR_SIZE = {
-  sm: "w-6 h-6",
-  md: "w-8 h-8",
-  lg: "w-12 h-12",
-  xxxxl: "w-32 h-32",
+  sm: { class: "w-6 h-6", size: 24 },
+  md: { class: "w-8 h-8", size: 32 },
+  lg: { class: "w-12 h-12", size: 48 },
+  xl: { class: "w-16 h-16", size: 64 },
+  xxl: { class: "w-20 h-20", size: 80 },
+  xxxl: { class: "w-24 h-24", size: 96 },
+  xxxxl: { class: "w-32 h-32", size: 128 },
 } as const;
 
 export type AvatarSize = keyof typeof AVATAR_SIZE;
@@ -21,16 +24,12 @@ interface AvatarProps {
 
 const Avatar = ({ user, size = "md", className = "" }: AvatarProps) => {
   const [imgError, setImgError] = useState(false);
+  const [isClient, setIsClient] = useState(false);
 
-  const sizeClasses = {
-    sm: "w-6 h-6",
-    md: "w-8 h-8",
-    lg: "w-12 h-12",
-    xl: "w-16 h-16",
-    xxl: "w-20 h-20",
-    xxxl: "w-24 h-24",
-    xxxxl: "w-32 h-32",
-  };
+  // 클라이언트 사이드에서만 실행되는 코드
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
   const handleError = () => {
     setImgError(true);
@@ -38,7 +37,7 @@ const Avatar = ({ user, size = "md", className = "" }: AvatarProps) => {
   const { username, image } = user || {};
   const defaultImage = `https://ui-avatars.com/api/?name=${username}&format=png`;
 
-  const imageUrl = imgError || !image ? defaultImage : image;
+  const imageUrl = (isClient && imgError) || !image ? defaultImage : image;
 
   return (
     <Image
@@ -46,9 +45,10 @@ const Avatar = ({ user, size = "md", className = "" }: AvatarProps) => {
       alt={`${username}'s avatar`}
       width={24}
       height={24}
-      className={`rounded-full object-cover ${sizeClasses[size]} ${className}`}
+      className={`rounded-full object-cover ${AVATAR_SIZE[size].class} ${className}`}
+      sizes={`${AVATAR_SIZE[size].size}px`}
       onError={handleError}
-      quality={75}
+      quality={100}
     />
   );
 };
